6ca9c56a42799f48bf31b18e6bbca4e6fca02769
Alex Akulov <[alexyiyi92@gmail.com]>
2022-02-09 16:00:43 -0500

    Sasha's solution for Findmax Number in Array task

--- src/array/Array_FindMaximum.java
+++ src/array/Array_FindMaximum.java
@@ -6,7 +6,16 @@ public class Array_FindMaximum {
         int[] arr = {0,5,6,9,7,2,-5,2,2-99,22,0};
 
 
+        System.out.println(returnMax(arr));
     }
-
+     public static int returnMax(int [] arr){
+        int max = arr[0];
+         for (int i = 0; i < arr.length; i++) {
+             if (arr[i]>max){
+                 max=arr[i];
+             }
+         }
+return max;
+    }
 }
 //Write a function that can find the maximum number from an int Array
