06390b648c0684536c60e29338dfa1a139058472
Alex Akulov <[alexyiyi92@gmail.com]>
2022-02-09 18:19:48 -0500

    Sasha's solution for Findmin Number in Array task

--- src/array/Array_FindMinimum.java
+++ src/array/Array_FindMinimum.java
@@ -3,7 +3,18 @@ package array;
 public class Array_FindMinimum {
     public static void main(String[] args) {
 
-        int[] arr = {0,5,6,9,7,2,-5,2,2-99,22,0};
+        int[] arr = {-100, 5, 6, 9, 7, 2, -5, 2, 2, - 99, 22, 0};
+        System.out.println(returnMin(arr));
+    }
+
+    public static int returnMin(int[] arr) {
+        int min = arr[0];//default array value index 0; variable max  can contain only 1 number;
+        for (int i = 1; i < arr.length; i++) { //iteration from 0 to the last index
+            if (arr[i] < min) { // iteration will be implemented from the first to the last element and will be finished
+                min = arr[i]; // it will place max value element into variable max
+            }
+        }
+        return min; //return max value;
     }
 }
 // Write a function that can find the minimum number from an int Array
