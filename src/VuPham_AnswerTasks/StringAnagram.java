fec09e95fb0e9432aae4a23f4722699ad76eb9a1
vupha <vupham129@gmail.com>
2022-02-16 13:07:27 -0800

    VuPham_AnswerStringTasks

---
+++ src/VuPham_AnswerTasks/StringAnagram.java
@@ -0,0 +1,12 @@
+package VuPham_AnswerTasks;
+
+public class StringAnagram {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+Write a  function that check if a string is build out of the same letters as another string.
+        Ex: same("abc", "cab"); -> true , Solution 1:
+            same("abc", "abb"); -> false:
+ */
